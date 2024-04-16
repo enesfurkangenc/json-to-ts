0478173d67aaf509d8069a9c269fe79d87798f39
Enes GENC <enecgenc@gmail.com>
2024-04-17 00:18:21 +0300

    feat: change keymap

--- lua/json-to-ts/init.lua
+++ lua/json-to-ts/init.lua
@@ -5,7 +5,7 @@ local util = require("json-to-ts.util")
 function M.setup(opts)
    opts = opts or {}
 
-   vim.keymap.set("n", "<leader>d", function()
+   vim.keymap.set("n", "<leader>h", function()
       util.convert_json_to_ts()
    end, { desc = "Json to TS" })
 end
