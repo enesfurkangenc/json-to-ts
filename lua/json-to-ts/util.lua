e34253aaa501586fc5dc1e6cc5a04460cd9f5f67
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:48:43 +0300

    feat: test 3

--- lua/json-to-ts/util.lua
+++ lua/json-to-ts/util.lua
@@ -7,7 +7,7 @@ end
 
 function M.show_clipboard_content()
    local clipboard_content = M.get_clipboard_content()
-   print("test :" + clipboard_content)
+   print(clipboard_content)
    vim.api.nvim_out_write("Panodaki İçerik: " .. clipboard_content .. "\n")
 end
 
