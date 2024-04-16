21e2a910ea79caf88786012160cc57311e490691
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:05:16 +0300

    feat: fix typo

--- lua/json-to-ts/init.lua
+++ lua/json-to-ts/init.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup(opts)
    opts = opts or {}
 
-   vim.keymap.set("n", "<leader>h", function()
+   vim.keymap.set("n", "<Leader>h", function()
       if opts.name then
          print("Hello, " .. opts.name)
       else
