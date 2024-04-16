bcf6318705c43c1321558d6971ccc899bc2c0667
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:32:58 +0300

    test clear

--- lua/json-to-ts/init.lua
+++ lua/json-to-ts/init.lua
@@ -3,16 +3,11 @@ local M = {}
 function M.setup(opts)
    opts = opts or {}
 
-   print("json to ts loaded")
-   print("load 1")
    vim.keymap.set("n", "<leader>d", function()
-      print("load 2")
       if opts.name then
          print("hello, " .. opts.name)
       else
          print("hello")
-
-         print("load 3")
       end
    end, { desc = "Json to TS" })
 end
