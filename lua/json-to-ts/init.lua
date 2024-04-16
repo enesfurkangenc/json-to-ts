d9db6c60d077507ef0f971385cdf4849c1ceedae
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:12:40 +0300

    fix: change typos

--- lua/json-to-ts/init.lua
+++ lua/json-to-ts/init.lua
@@ -3,13 +3,14 @@ local M = {}
 function M.setup(opts)
    opts = opts or {}
 
-   vim.keymap.set("n", "<Leader>d", function()
+   print("json to ts loaded")
+   vim.keymap.set("n", "<leader>d", function()
       if opts.name then
          print("hello, " .. opts.name)
       else
          print("hello")
       end
-   end)
+   end, { desc = "Json to TS" })
 end
 
 return M
