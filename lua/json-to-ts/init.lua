a8df872cebcf0b80650e75ce9d63ffe19dc0dd04
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:41:54 +0300

    feat: util

--- lua/json-to-ts/init.lua
+++ lua/json-to-ts/init.lua
@@ -1,9 +1,12 @@
 local M = {}
 
+local util = require("json-to-ts.util")
+
 function M.setup(opts)
    opts = opts or {}
 
    vim.keymap.set("n", "<leader>d", function()
+      util.show_clipboard_content()
       if opts.name then
          print("hello, " .. opts.name)
       else
---
+++ lua/json-to-ts/util.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+function M.get_clipboard_content()
+   local clipboard_content = vim.fn.getreg("+")
+   return clipboard_content
+end
+
+function M.show_clipboard_content()
+   local clipboard_content = M.get_clipboard_content()
+   print(clipboard_content)
+   vim.api.nvim_out_write("Panodaki İçerik: " .. clipboard_content .. "\n")
+end
+
+return M
