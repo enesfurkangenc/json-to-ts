cb88ba9874e18f2e5a76675538ad503af6fb19b7
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:53:29 +0300

    feat: put file

--- lua/json-to-ts/util.lua
+++ lua/json-to-ts/util.lua
@@ -7,8 +7,7 @@ end
 
 function M.show_clipboard_content()
    local clipboard_content = M.get_clipboard_content()
-   print(clipboard_content)
-   vim.api.nvim_out_write("Panodaki İçerik: " .. clipboard_content .. "\n")
+   vim.api.nvim_put({ clipboard_content }, "c", true, true)
 end
 
 return M
