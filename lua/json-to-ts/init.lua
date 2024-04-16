dffb8b2807f6a87b3db9c294e160fd167e57bc04
Enes GENC <enecgenc@gmail.com>
2024-04-16 23:09:41 +0300

    fix: change leader

--- lua/json-to-ts/init.lua
+++ lua/json-to-ts/init.lua
@@ -3,11 +3,11 @@ local M = {}
 function M.setup(opts)
    opts = opts or {}
 
-   vim.keymap.set("n", "<Leader>h", function()
+   vim.keymap.set("n", "<Leader>d", function()
       if opts.name then
-         print("Hello, " .. opts.name)
+         print("hello, " .. opts.name)
       else
-         print("Hello, world!")
+         print("hello")
       end
    end)
 end
